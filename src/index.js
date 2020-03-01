3efbe6f3cc7c5e9c885a4912a8cb5c5070b6a312
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-03-01 23:30:39 +0100

    Get map and robots info & aux input

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const cli = require('./utils/cli');
 const transcriber = require('./utils/transcriber')
 
+let inputAux ='5 3\n1 1 E\nRFRFRFRF\n3 2 N\nFRRFLLFFRRFLL\n0 3 W\nLLFFFLFLFL';
+
 const startMission = async () => {
-    const input = await cli.getInput();
-    const missionInfo = transcriber.getMissionInfo(input);
-    console.log(missionInfo);
+    // const input = await cli.getInput();
+    const missionInfo = transcriber.getMissionInfo(inputAux);
+    console.log(missionInfo.map, missionInfo.robots);
 };
 
 startMission();
--- src/utils/transcriber.js
+++ src/utils/transcriber.js
@@ -1,6 +1,11 @@
 const getMissionInfo = input => {
     const lines = input.split('\n');
-    const map = getMapDimensions(lines[0]);
+    const map = getMapDimensions(lines.shift());
+    const robots = getRobots(lines);
+    return {
+        map,
+        robots
+    };
 };
 
 const getMapDimensions = line => {
@@ -8,6 +13,21 @@ const getMapDimensions = line => {
         width: Number(line.split(' ')[0]),
         height: Number(line.split(' ')[1])
     };
+};
+
+const getRobots = lines => {
+    const robots = [];
+    for (let i = 0; i < lines.length - 1; i += 2) {
+        robots.push({
+            coords: {
+                x: Number(lines[i].split(' ')[0]),
+                y: Number(lines[i].split(' ')[1])
+            },
+            orientation: lines[i].split(' ')[2],
+            instructions: lines[i + 1].split('')
+        });
+    }
+    return robots;
 };
 
 module.exports = {
